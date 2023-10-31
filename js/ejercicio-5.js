46c7fa2b00bb7e6cf06a31648e547aaa5da2d13d
Franco <lmassa.fms@gmail.com>
2023-10-31 10:57:11 -0300

    realizo ejercicio 5

---
+++ js/ejercicio-5.js
@@ -0,0 +1,6 @@
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+const numero1 = parseInt(prompt('Ingrese primer número'));
+const numero2 = parseInt(prompt('Ingrese segundo número'));
+
+const resultado = document.write(`El resultado de sumar ${numero1} mas ${numero2} es: ${numero1+numero2}`);
