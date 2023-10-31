112008ecacee259a0780e098998fc515ec12ad11
Franco <lmassa.fms@gmail.com>
2023-10-31 13:13:10 -0300

    realizo ejercicio 10

---
+++ js/ejercicio-9.js
@@ -0,0 +1,16 @@
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+const frase = prompt('Ingrese una frase');
+
+console.log(frase.length)
+// console.log(frase.charAt(0)); 
+// console.log(frase.substring(0,3));
+vocales = "";
+
+for (let letra = 0; letra < frase.length; letra++) {
+    console.log(letra);
+    if ('aeiouAEIOU'.includes(frase)) {
+        vocales += letra;
+        console.log(vocales);
+    }
+}
